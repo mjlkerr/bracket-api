01da80c33d28c8a7f134a23fb9d4869e3c2d5ac9
Martha Kerr <mjlkerr77@gmail.com>
2021-06-22 07:34:45 -0400

    start of controller for bracket

---
+++ bracket-api/Controllers/BracketController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using bracket_api.Models;
+
+namespace bracket_api.Controllers
+{
+    public class BracketController : ControllerBase
+    {
+        private readonly BracketContext _context;
+
+        public BracketController(BracketContext context)
+        {
+            _context = context;
+        }
+    }
+}
